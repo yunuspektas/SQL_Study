0e1e019fb7b4cf45456461beb02cab8a7c1e91f8
Yunus Pektas <yunuspektas@protonmail.com>
2022-05-27 20:00:58 +0300

    05.27.2002

--- task_01 hastane_otomasyon.sql
+++ task_01 hastane_otomasyon.sql
@@ -1,2 +1,67 @@
 /* ========================== HASTA PROJESI =================================*/   
+create table if not exists hastaneler(    /* if not */
+id char(5) primary key,
+isim varchar(50),
+sehir varchar(10),
+ozel varchar(1)
+-- ozel boolean default true
+
+);  
+
+insert into hastaneler values('H101','acıbadem hastanesi','istanbul','y');
+insert into hastaneler values('H102','hazseki hastanesi','istanbul','n');
+insert into hastaneler values('H103','medikol hastanesi','izmir','y');
+insert into hastaneler values('H104','memorial hastanesi','ankara','y');
+
+create table if not exists hastalar
+(
+kimlik_no varchar(11) primary key,
+isim varchar(30),
+teshis varchar(30)
+);
+insert into hastalar values ('12345678901','Ali Can','Gizli Seker');
+insert into hastalar values ('45678901121','Ayşe Yılmaz','Hipertansiyon');
+insert into hastalar values ('78901123451','Steve Job','Pankreatit');
+insert into hastalar values ('12344321251','Tom Hanks','COVID19');
+
+
+create table if not exists bolumler
+(
+bolum_id char(5) primary key,
+bolum_adi varchar(30)
+);
+
+insert into bolumler values ('DHL','DAHILIYE');
+insert into bolumler values ('KBB','KULAK-BURUN-BOGAZ');
+insert into bolumler values ('NRJ','NOROLOJI');
+insert into bolumler values ('GAST','GASTROLOJI');
+insert into bolumler values ('KARD','KARDIOLOJI');
+insert into bolumler values ('PSK','PSIKIYATRI');
+insert into bolumler values ('GOZ','GOZ HASTALIKLARI');
+
+
+
+create table if not exists hasta_kayitlar
+(
+kimlik_no char(5) primary key,
+hast_isim varchar(30) default 'NONAME', -- default değeri noname
+hastane_adi varchar(30) NULL, -- null olabilir demek
+bolum_adi varchar(30) NULL,
+teshis varchar(30) NULL
+);
+
+insert into hasta_kayitlar(kimlik_no) values('1111'); -- trick sadece girilecek field belirtilmiş
+insert into hasta_kayitlar(kimlik_no) values('2222');
+insert into hasta_kayitlar(kimlik_no) values('3333');
+
+select * from hastaneler;
+select * from hasta_kayitlar;
+select * from hastalar;
+select * from bolumler;
+
+
+
+
+
+
    
--- task_2.sql
+++ task_2.sql
@@ -15,14 +15,45 @@ calisanlar tablosu --> id, isim, maas
  create ediniz
 */
 
+create table calisanlar (
+id char(4),
+isim varchar(50),
+maas int(5),
+constraint id_pk primary key (id)
+);
+insert into calisanlar values ( '1001', 'Ahmet Aslan', 7000);
+insert into calisanlar values ( '1002', 'Mehmet Yılmaz' ,12000);
+insert into calisanlar values ('1003', 'Meryem ', 7215);
+insert into calisanlar values ('1004', 'Veli Han', 5000);
+
+select * from calisanlar;
+
+create table aileler (
+id char(4),
+cocuk_sayisi varchar(20),
+ek_gelir int(20),
+constraint id_fk foreign key (id) references calisanlar (id)
+);
+
+insert into aileler values ('1001', 4, 2000);
+insert into aileler values ('1002', 2 ,1500);
+insert into aileler values ('1003', 1, 2200);
+insert into aileler values ('1004', 3, 2400);
+
+select * from aileler;
+SET SQL_SAFE_UPDATES = 0;
+
 
  -- 2) Veli Han'ın maaşına %20 zam yapınız.
-    
+ update calisanlar set maas = maas*1.2 where isim='Veli Han';   
+select avg(maas) from calisanlar;
     
  -- 3) Maaşı ortalamanın altında olanlara %20 zam yapınız.
-    
+ update calisanlar set maas = maas*1.2  WHERE maas < (select *  from (SELECT AVG(maas) FROM calisanlar) yeni);
+          -- bu soruda cevap kısmına alias olarak bir isim ( bu örnekde "yeni" yazıldı) eklemez isek hata alıyoruz
                     
 -- 4) calisanların isim ve cocuk_sayisi'ni listeyen sorguyu yazınız.
+select isim,aileler.cocuk_sayisi from calisanlar where id=aileler.id;
 		
             
 -- 5) calisanlar' ın  isim ve toplam_gelir'lerini gösteren bir sorgu yazınız. 
